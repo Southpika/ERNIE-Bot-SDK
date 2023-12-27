73482e7e257c018c1f99b0119093fc257cf4a77e
Southpika <513923576@qq.com>
2023-12-27 13:34:35 +0800

    save

--- erniebot-agent/tests/integration_tests/agents/test_gradio_agent.py
+++ erniebot-agent/tests/integration_tests/agents/test_gradio_agent.py
@@ -1,21 +1,17 @@
-import os
-
-from erniebot_agent.agents.functional_agent import FunctionalAgent
+from erniebot_agent.agents import FunctionAgent
 from erniebot_agent.chat_models import ERNIEBot
-from erniebot_agent.file.file_manager import FileManager
 from erniebot_agent.memory import WholeMemory
 from erniebot_agent.tools import RemoteToolkit
-from erniebot_agent.tools.mimic_imgtool import ImageSegmentTool
 from erniebot_agent.tools.tool_manager import ToolManager
 
 
 async def image_moderation():
     tools = []
     # url = "https://tool-image-moderation.aistudio-hub.baidu.com"
-    # toolkit = RemoteToolkit.from_url(url, version="v1.2", access_token="1dc43e5843cfb51b7b41ba766aff2372cf2f3ccb")
+    # toolkit = RemoteToolkit.from_url(access_token="1dc43e5843cfb51b7b41ba766aff2372cf2f3ccb")
     # tools.extend(toolkit.get_tools())
     # url = "https://tool-highacc-ocr.aistudio-hub.baidu.com"
-    # toolkit = RemoteToolkit.from_url(url, version="v1.3", access_token="1dc43e5843cfb51b7b41ba766aff2372cf2f3ccb")
+    # toolkit = RemoteToolkit.from_url( access_token="1dc43e5843cfb51b7b41ba766aff2372cf2f3ccb")
     # tools.extend(toolkit.get_tools())
     # url = "https://tool-img-enhance.aistudio-hub.baidu.com"
     # toolkit = RemoteToolkit.from_url(url, access_token="1dc43e5843cfb51b7b41ba766aff2372cf2f3ccb")
@@ -23,22 +19,20 @@ async def image_moderation():
     # url = "http://tool-texttospeech.sandbox-aistudio-hub.baidu.com"
     # toolkit = RemoteToolkit.from_url(url, access_token="1dc43e5843cfb51b7b41ba766aff2372cf2f3ccb")
     # tools.extend(toolkit.get_tools())
-    url = "http://tool-pp-structure-v2.sandbox-aistudio-hub.baidu.com"
-    toolkit = RemoteToolkit.from_url(url, access_token="1dc43e5843cfb51b7b41ba766aff2372cf2f3ccb")
+    # url = "http://tool-pp-structure-v2.sandbox-aistudio-hub.baidu.com"
+    toolkit = RemoteToolkit.from_aistudio("texttospeech")
     tools.extend(toolkit.get_tools())
     # url = "https://dfn9ed87r7138fi3.aistudio-hub.baidu.com"
-    # url = 'http://tool-pp-human-v2.sandbox-aistudio-hub.baidu.com'   
+    # url = 'http://tool-pp-human-v2.sandbox-aistudio-hub.baidu.com'
     # toolkit = RemoteToolkit.from_url(url, access_token="1dc43e5843cfb51b7b41ba766aff2372cf2f3ccb")
     # tools.extend(toolkit.get_tools())
     # url = "https://yfo319edw9s7d2t6.aistudio-hub.baidu.com"
     # toolkit = RemoteToolkit.from_url(url, access_token="4ce50e3378f418d271c480c8ddfa818537071dbe")
     # tools.extend(toolkit.get_tools())
 
-    llm = ERNIEBot(
-        model="ernie-bot", api_type="custom"
-    )
+    llm = ERNIEBot(model="ernie-3.5", api_type="aistudio", enable_multi_step_tool_call=True)
 
-    agent = FunctionalAgent(
+    agent = FunctionAgent(
         llm=llm,
         tools=ToolManager(tools=tools),
         memory=WholeMemory(),
